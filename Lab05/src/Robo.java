249b629bd91801c9f7ce67bd04a8b1221fc922f1
Felipe <gouveia.fsg@gmail.com>
2018-05-23 22:50:49 -0300

    Lab05

--- Lab05/src/Robo.java
+++ Lab05/src/Robo.java
@@ -7,6 +7,9 @@ public class Robo {
     private Sala sala=null;
     private int energia, passos=0;
     
+    /**
+     *  Aloca o robo criado na primeira posicao livre da sala
+     */
     private boolean setPosicaoInicial() throws Exception{
         for(int i=0;i<this.sala.getNumPosicoesHorizontais();i++){
             for(int j=0;j<this.sala.getNumPosicoesVerticais();j++){
@@ -21,6 +24,12 @@ public class Robo {
         return false;
     }
 
+    /**
+     *  Cria um objeto robo
+     * @param sala endereco da sala designada
+     * @param energia capacidade do robo de se locomover pela sala
+     * @throws Exception Retorna uma excecao se os parametros estiverem abaixo do desejado ou se nao existir uma sala
+     */
     public Robo(Sala sala, int energia) throws Exception{
         if(energia<=0) throw new Exception("O Robo nao pode ter ser inicializado com energia negativa ou nula..");
         if(sala==null) throw new Exception("Deve ser criada uma sala antes de criar um Robo..");
@@ -29,10 +38,19 @@ public class Robo {
         this.energia = energia;
     }
     
+    /**
+     *  Verifica se o robo possui energia para se mover
+     * @return Retorna se a energia eh maior que zero ou nao
+     */
     public boolean existeEnergia(){
         return energia>0;
     }
     
+    /**
+     *  Movimenta o robo para cima
+     * @return Retorna se o movimento foi bem sucedido ou nao
+     * @throws Exception Retorna uma excecao se o robo nao tiver energia
+     */
     public boolean subir() throws Exception{
         if(!existeEnergia()) throw new Exception("Robo sem energia..");
         if(sala.posicaoValida(posicaoRobo[0]-1, posicaoRobo[1])&&sala.isPosicaoLivre(posicaoRobo[0]-1, posicaoRobo[1])){
@@ -45,6 +63,11 @@ public class Robo {
         return false;
     }
     
+    /**
+     *  Movimenta o robo para baixo
+     * @return Retorna se o movimento foi bem sucedido ou nao
+     * @throws Exception Retorna uma excecao se o robo nao tiver energia
+     */
     public boolean descer() throws Exception{
         if(!existeEnergia()) throw new Exception("Robo sem energia..");
         if(sala.posicaoValida(posicaoRobo[0]+1, posicaoRobo[1])&&sala.isPosicaoLivre(posicaoRobo[0]+1, posicaoRobo[1])){
@@ -57,6 +80,11 @@ public class Robo {
         return false;
     }
     
+    /**
+     *  Movimenta o robo para a esquerda
+     * @return Retorna se o movimento foi bem sucedido ou nao
+     * @throws Exception Retorna uma excecao se o robo nao tiver energia
+     */
     public boolean esquerda() throws Exception{
         if(!existeEnergia()) throw new Exception("Robo sem energia..");
         if(sala.posicaoValida(posicaoRobo[0], posicaoRobo[1]-1)&&sala.isPosicaoLivre(posicaoRobo[0], posicaoRobo[1]-1)){
@@ -69,6 +97,11 @@ public class Robo {
         return false;
     }
     
+    /**
+     *  Movimenta o robo para a direita
+     * @return Retorna se o movimento foi bem sucedido ou nao
+     * @throws Exception Retorna uma excecao se o robo nao tiver energia
+     */
     public boolean direita() throws Exception{
         if(!existeEnergia()) throw new Exception("Robo sem energia..");
         if(sala.posicaoValida(posicaoRobo[0], posicaoRobo[1]+1)&&sala.isPosicaoLivre(posicaoRobo[0], posicaoRobo[1]+1)){
@@ -81,18 +114,37 @@ public class Robo {
         return false;
     }
 
+    /**
+     *  Confirma se o robo esta em uma determinada posicao da sala
+     * @param POSICAO_LINHA Coordenada linha
+     * @param POSICAO_COLUNA Coordenada coluna
+     * @return Retorna se a verificacao foi bem sucedida ou nao
+     */
     public boolean confirmarPosicaoAtual(int POSICAO_LINHA, int POSICAO_COLUNA) {
         return posicaoRobo[0]==POSICAO_LINHA&&posicaoRobo[1]==POSICAO_COLUNA;
     }
 
+    /**
+     *  Mostra o valor atual de energia do robo
+     * @return Retorna o valor da energia
+     */
     public int getEnergia() {
         return this.energia;
     }
 
+    /**
+     *  Mostra quantos passos o robo deu na sua sala
+     * @return Retorna a quantidade de passos
+     */
     public int getPassos() {
         return this.passos;
     }
     
+    /**
+     * Compara dois robos
+     * @param outroRobo Objeto a ser comparado
+     * @return Retorna se os robos examinados sao iguais ou nao
+     */
     public boolean equals(Robo outroRobo){
         return this.sala.equals(outroRobo.sala)&&this.posicaoRobo[0]==outroRobo.posicaoRobo[0]&&this.posicaoRobo[1]==outroRobo.posicaoRobo[1];
     }
--- Lab05/src/Sala.java
+++ Lab05/src/Sala.java
@@ -8,6 +8,9 @@ public class Sala{
     private final int NUM_COLUNAS;
     private int sala[][];
     
+    /**
+     *  Esvazia a nova sala criada
+     */
     private void zerarSala(){
         for(int i=0;i<this.NUM_LINHAS;i++){
             for(int j=0;i<this.NUM_COLUNAS;j++){
@@ -16,6 +19,12 @@ public class Sala{
         }
     }
     
+    /**
+     *  Cria um objeto sala
+     * @param NUM_LINHAS Numero de linhas da sala
+     * @param NUM_COLUNAS Numero de colunas da sala
+     * @throws Exception Retorna uma excecao se os parametros estiverem abaixo do desejado
+     */
     public Sala(int NUM_LINHAS, int NUM_COLUNAS) throws Exception{
         if(((NUM_LINHAS<=0)&&(NUM_COLUNAS<=0))||NUM_LINHAS<=0||NUM_COLUNAS<=0) throw new Exception("Parametros incorretos para a criacao da sala. As dimensoes da sala devem ser representadas por valores positivos maiores que zero");       
         
@@ -25,14 +34,26 @@ public class Sala{
         this.NUM_COLUNAS = NUM_COLUNAS;
     }
 
+    /**
+     *  Mostra o numero de linhas da sala
+     * @return Retorna o numero de linhas
+     */
     public int getNumPosicoesHorizontais() {
         return this.NUM_LINHAS;
     }
 
+    /**
+     *  Mostra o numero de colunas da sala
+     * @return Retorna o numero de colunas
+     */
     public int getNumPosicoesVerticais() {
         return this.NUM_COLUNAS;
     }
 
+    /**
+     *Mostra o estado da sala
+     * @return Retorna se a sala esta vazia ou nao
+     */
     public boolean isVazia() {
         for(int i=0;i<this.NUM_LINHAS;i++){
             for(int j=0;j<this.NUM_COLUNAS;j++){
@@ -42,6 +63,12 @@ public class Sala{
         return true;
     }
 
+    /**
+     *  Insere um obstaculo na sala
+     * @param POSICAO_LINHA Coordenada linha
+     * @param POSICAO_COLUNA Coordenada coluna
+     * @return Retorna se a insercao do obstaculo foi bem sucedida ou nao
+     */
     public boolean inserirObstaculo(int POSICAO_LINHA, int POSICAO_COLUNA) {
         if(POSICAO_LINHA<0||POSICAO_COLUNA<0||POSICAO_LINHA>=this.NUM_LINHAS||POSICAO_COLUNA>=this.NUM_COLUNAS){
             return false;
@@ -50,21 +77,46 @@ public class Sala{
         return true;
     }
 
+    /**
+     *  Verifica se uma determinada coordenada da sala eh valida
+     * @param POSICAO_LINHA Coordenada linha
+     * @param POSICAO_COLUNA Coordenada coluna
+     * @return Retorna se a posicao examinada eh valida ou nao
+     */
     public boolean posicaoValida(int POSICAO_LINHA, int POSICAO_COLUNA) {
         return !(POSICAO_LINHA<0||POSICAO_COLUNA<0||POSICAO_LINHA>=this.NUM_LINHAS||POSICAO_COLUNA>=this.NUM_COLUNAS);
     }
 
+    /**
+     *  Verifica se uma determinada posicao da sala esta livre
+     * @param POSICAO_LINHA Coordenada linha
+     * @param POSICAO_COLUNA Coordenada coluna
+     * @return Retorna se a posicao examinada possui um obstaculo ou nao
+     * @throws Exception Retorna uma excecao se a posicao examinada eh valida ou nao
+     */
     public boolean isPosicaoLivre(int POSICAO_LINHA, int POSICAO_COLUNA) throws Exception{
         if(posicaoValida(POSICAO_LINHA, POSICAO_COLUNA)==false) throw new Exception("Posicao inexistente.");
         return (sala[POSICAO_LINHA][POSICAO_COLUNA] == LIVRE);
     }
 
-    boolean setPosicao(int POSICAO_LINHA, int POSICAO_COLUNA, int opcao) {
+    /**
+     *  Muda o estado de uma posicao da sala
+     * @param POSICAO_LINHA Coordenada linha
+     * @param POSICAO_COLUNA Coordenada coluna
+     * @param opcao Indica a insercao de um obstaculo ou o esvaziamento da posicao examinada 
+     * @return
+     */
+    public boolean setPosicao(int POSICAO_LINHA, int POSICAO_COLUNA, int opcao) {
         if(posicaoValida(POSICAO_LINHA, POSICAO_COLUNA)==false) return false;
         sala[POSICAO_LINHA][POSICAO_COLUNA]=opcao;
         return true;
     }
     
+    /**
+     *  Compara duas salas
+     * @param outraSala Objeto a ser comparado
+     * @return Retorna se as salas examinadas sao iguais ou nao
+     */
     public boolean equals(Sala outraSala){
         for(int i=0;i<this.NUM_LINHAS;i++){
             for(int j=0;j<this.NUM_COLUNAS;j++){
