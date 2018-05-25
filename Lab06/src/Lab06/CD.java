4d2d03beff6db23ec4d0b4b4c26f38a3e371fa5f
Felipe <gouveia.fsg@gmail.com>
2018-05-24 23:18:24 -0300

    Lab06 em progresso..

--- Lab06/src/Lab06/CD.java
+++ Lab06/src/Lab06/CD.java
@@ -4,25 +4,82 @@
 
 package Lab06;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CD {
-    String artista;
-    String titulo;
-    String trilhaPrincipal;
-    final int M;
-    List<String> faixas;
+    private final String ARTISTA;
+    private final String TITULO;
+    private String trilhaPrincipal;
+    private final int M;
+    private List<String> album;
+    private int slotAlbum=0;
     
-    public CD(String titulo, String artista, int M){
-        this.titulo = titulo;
-        this.artista = artista;
+    public CD(String TITULO, String ARTISTA, int M) throws Exception{
+        if(TITULO==null||ARTISTA==null) throw new Exception("Título e/ou artista não podem ser vazios.");
+        album = new ArrayList<>();
+        this.TITULO = TITULO;
+        this.ARTISTA = ARTISTA;
         this.M = M;
     }
     
-    public CD(String titulo, String artista){
-        this.titulo = titulo;
-        this.artista = artista;
+    public CD(String TITULO, String ARTISTA) throws Exception{
+        if(TITULO==null||ARTISTA==null) throw new Exception("Título e/ou artista não podem ser vazios.");
+        album = new ArrayList<>();
+        this.TITULO = TITULO;
+        this.ARTISTA = ARTISTA;
         this.M = 10;
+    }
+
+    public String getArtista() {
+        return ARTISTA;
+    }
+
+    public String getTitulo() {
+        return TITULO;
+    }
+
+    public String getTrilhaPrincipal() {
+        return trilhaPrincipal;
+    }
+    
+    public int getM() {
+        return M;
+    }
+    
+    public void setTrilhaPrincipal(String trilhaPrincipal) throws Exception {
+        for(int i=0;i<album.size();i++){
+            String faixa = album.get(i);
+            if(faixa.equals(this.trilhaPrincipal)) {
+                this.trilhaPrincipal = trilhaPrincipal;
+                break;
+            }
+        }
+        throw new Exception("Faixa nao consta no album");
+    }
+
+    public String getFaixa(int numFaixa){
+        if(numFaixa<=0||numFaixa>this.M) return null;
+        return album.get(numFaixa);
+    }
+    
+    public boolean cadastroFaixa(String faixa){
+        if(this.slotAlbum<this.M){
+            album.add(this.slotAlbum, faixa);
+            this.slotAlbum++;
+            return true;
+        }
+        return false;
+    }
+    
+    @Override
+    public String toString(){
+        return "ARTISTA: "+this.ARTISTA+"\nTITULO: "+this.TITULO+"\nTRILHA PRINCIPAL: "+this.trilhaPrincipal+"\n";
+    }
+    
+    public boolean equals(CD cd){
+        if(this.ARTISTA.equals(cd.ARTISTA)&&this.TITULO.equals(cd.TITULO)) return true;
+        return false;
     }
     
 }
